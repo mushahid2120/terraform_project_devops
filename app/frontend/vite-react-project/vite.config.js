f6a09ebe58397ee2fa6c6d1f63bc8899275d170e
Md Mushahid Ansari <142341806+mushahid2120@users.noreply.github.com>
2025-04-28 19:00:05 +0530

    Update vite.config.js

--- app/frontend/vite-react-project/vite.config.js
+++ app/frontend/vite-react-project/vite.config.js
@@ -5,14 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    'host': true,
     proxy: {
       '/submit': {
-        target: `http://${process.env.ALB_ENDPOINT}:${process.env.ALB_ENDPOINT}`,
+        target: `http://${process.env.ALB_ENDPOINT}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/submit/, '/submit'),
       },
       '/fetch': {
-        target: `http://${process.env.ALB_ENDPOINT}:${process.env.ALB_ENDPOINT}`,
+        target: `http://${process.env.ALB_ENDPOINT}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/fetch/, '/fetch'),
       },
